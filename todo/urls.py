2e6be13e9034ada82c296102dc3140fc5e97f81a
Naeim Salib <naeimsalib@yahoo.com>
2024-07-14 19:06:24 -0400

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TodoListCreate, TodoDetail
+from .views import TodoListCreate, TodoDetail, health_check
 
 urlpatterns = [
     path('api/todos/', TodoListCreate.as_view(), name='todo_list_create'),
     path('api/todos/<int:pk>/', TodoDetail.as_view(), name='todo_detail'),
+    path('', health_check, name='health_check'),
 ]
