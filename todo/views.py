10d137e120d4bb8e01dd1879aa0a558188bdb680
Naeim Salib <naeimsalib@yahoo.com>
2024-07-14 19:06:44 -0400

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from .models import Todo
 from .serializers import TodoSerializer
+from django.http import JsonResponse
+
 
 class TodoListCreate(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
@@ -9,3 +11,6 @@ class TodoListCreate(generics.ListCreateAPIView):
 class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+def health_check(request):
+    return JsonResponse({'status': 'ok'}, status=200)
