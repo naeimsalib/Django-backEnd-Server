76d79744725da60fe1f10e835545fb9a35acc570
Naeim Salib <naeimsalib@yahoo.com>
2024-07-15 14:19:43 -0400

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from todo.views import health_check
-from . import views
-
+from todo.views import health_check, TodoListCreate, TodoDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('todo.urls')),
     path('', health_check, name='health_check'),
-    path('api/todos/', views.todos, name='todos'),
 ]
