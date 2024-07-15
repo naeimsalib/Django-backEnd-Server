0be8d3ec7e3292db2de692a7817a0b8d56fa0b94
Naeim Salib <naeimsalib@yahoo.com>
2024-07-15 14:07:29 -0400

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('todo.urls')),
     path('', health_check, name='health_check'),
-
+    path('api/todos/', views.todos, name='todos'),
 ]
