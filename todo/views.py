12faea2ff9d47f173abc8e8af210bb93639bba9a
Naeim Salib <naeimsalib@yahoo.com>
2024-07-15 20:35:07 -0400

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -1,15 +1,41 @@
+from django.http import JsonResponse
 from rest_framework import generics
 from .models import Todo
 from .serializers import TodoSerializer
-from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 class TodoListCreate(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
+    @method_decorator(csrf_exempt)
+    @method_decorator(require_http_methods(["GET", "POST", "OPTIONS"]))
+    def dispatch(self, *args, **kwargs):
+        response = super().dispatch(*args, **kwargs)
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        return response
+
 class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
+    @method_decorator(csrf_exempt)
+    @method_decorator(require_http_methods(["GET", "PUT", "DELETE", "OPTIONS"]))
+    def dispatch(self, *args, **kwargs):
+        response = super().dispatch(*args, **kwargs)
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Methods"] = "GET, PUT, DELETE, OPTIONS"
+        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        return response
+
+@require_http_methods(["GET", "OPTIONS"])
 def health_check(request):
-    return JsonResponse({'status': 'ok'}, status=200)
+    response = JsonResponse({'status': 'ok'}, status=200)
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
